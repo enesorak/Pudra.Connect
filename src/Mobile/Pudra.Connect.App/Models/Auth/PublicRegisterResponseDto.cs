178a82108d00f4f506432e4dd8d521bbb90b28fd
Enes Orak <73196279+enesorak@users.noreply.github.com>
2026-04-03 14:52:27 +0300

    Create PublicRegisterResponseDto.cs

---
+++ src/Mobile/Pudra.Connect.App/Models/Auth/PublicRegisterResponseDto.cs
@@ -0,0 +1,9 @@
+namespace Pudra.Connect.App.Models.Auth;
+
+public class PublicRegisterResponseDto
+{
+    public bool Success { get; set; }
+    public string Message { get; set; } = string.Empty;
+    
+    // yorum ekleyelim ki gitsin amk
+}
